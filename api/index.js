e47cce0bc03aa96977527b402ab729ef5794a4bf
Wesley Bliss <wesley.bliss@gmail.com>
2022-03-15 15:20:51 -0500

    Updated structure

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 const fs = require('fs')
 const path = require('path')
+const dotenv = require('dotenv')
 const ganache = require('ganache')
+
+if (!process.env.IS_CI) {
+    const env = path.resolve(__dirname, '../.env')
+    dotenv.config({ path: env })
+}
 
 const totalAccounts = parseInt(process.env.TOTAL_ACCOUNTS, 10)
 const unlockedAccounts = Array(totalAccounts).fill(0).reduce((acc, _, i) => `${acc} ${i}`, '')
+const accountKeysPath = path.resolve(__dirname, '..', process.env.ACCOUNT_KEYS_PATH)
+const dbPath = path.resolve(__dirname, '..', process.env.DB_PATH)
 
 const options = {
     server: {
@@ -18,11 +26,11 @@ const options = {
         mnemonic: process.env.MNEMONIC,
         totalAccounts,
         unlockedAccounts,
-        accountKeysPath: process.env.ACCOUNT_KEYS_PATH,
+        accountKeysPath,
         defaultBalance: parseInt(process.env.DEFAULT_BALANCE),
     },
     database: {
-        dbPath: process.env.DB_PATH,
+        dbPath,
     },
 }
 
@@ -36,8 +44,7 @@ server.listen(port, err => {
     console.log(`Ganache listening on port ${port}...`)
     
     if (process.env.LOG_ACCOUNTS) {
-        const wallets = path.resolve(__dirname, 'accounts.json')
-        const accounts = JSON.parse(fs.readFileSync(wallets, 'utf8'))
+        const accounts = JSON.parse(fs.readFileSync(accountKeysPath, 'utf8'))
         console.log('ACCOUNTS', JSON.stringify(accounts, null, 4))
     }
     
